4e1d4be7f5efaa009775a27d51dd91b93379a71c
Andre <andre.elias.vieira@gmail.com>
2020-04-25 18:47:57 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/manbbo/com/br/globoapp/megahack/MainActivity.kt
@@ -0,0 +1,12 @@
+package manbbo.com.br.globoapp.megahack
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
