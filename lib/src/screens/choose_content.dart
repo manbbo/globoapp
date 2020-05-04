43d873a89e7d3907a436a7698bb8de9ca837a2b5
Andre <andre.elias.vieira@gmail.com>
2020-05-04 12:19:31 -0300

    Added the choose class and homepage class

--- lib/src/screens/choose_content.dart
+++ lib/src/screens/choose_content.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/rendering.dart';
-import '../widgets/content.dart';
-import './content_view.dart';
-import 'package:flutter/foundation.dart';
+import 'homepage.dart';
 
 class ChooseContent extends StatefulWidget {
   @override
@@ -11,156 +7,84 @@ class ChooseContent extends StatefulWidget {
 }
 
 class _ChooseContentState extends State<ChooseContent> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-
-        ],
+  List<bool> disabled = [false,false,false,false,false,false,false,false,false,false];
+  Widget createButton(text, int i) {
+    //bool disabled = false;
+    String finalText;
+    return RaisedButton(
+    child: Text(this.disabled[i]? "Adicionado" : text ),
+      color: Colors.white,
+      onPressed: () => this.disabled[i] ? this.disabled[i] = false : this.disabled[i] = true,
+      highlightColor: Colors.grey,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: IconButton(icon: Icon(Icons.navigate_next, color: Colors.white, size: 20,),),
-        onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage())); },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final globalKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: globalKey,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            leading: IconButton(
-              icon: Icon(
-                Icons.list,
-                color: Colors.blue,
-                size: 40,
-              ),
-              onPressed: () {
-                globalKey.currentState.openDrawer();
-              },
-            ),
-            pinned: true,
-            //expandedHeight: 0.0,
-            floating: true,
-            backgroundColor: Colors.white,
-            automaticallyImplyLeading: true,
-            stretch: true,
-            flexibleSpace: FlexibleSpaceBar(
-              titlePadding: EdgeInsets.only(left: 30, top: 5, bottom: 10),
-              collapseMode: CollapseMode.parallax,
-              centerTitle: true,
-              title: Text.rich(TextSpan(
-                  text: "globo.com",
-                  style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue))),
-            ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(20),
+        child: PageView(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    createButton('Cultura', 0),
+                    createButton('Educação', 1),
+                    createButton('Saúde', 2),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    createButton('Lazer', 3),
+                    createButton('Radio', 4),
+                    createButton('Fantástico', 5),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    createButton('Carros', 6),
+                    createButton('Esporte', 7),
+                    createButton('Política', 8),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    createButton('Mundo', 9),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: IconButton(
+          icon: Icon(
+            Icons.navigate_next,
+            color: Colors.white,
+            size: 20,
           ),
-          SliverFillRemaining(
-            child: Container(
-              child: ListView(
-                padding: EdgeInsets.only(top: 3),
-                children: <Widget>[
-                  InkWell(
-                      child: MainContent(
-                        topicName: "Principais Noticias",
-                        newsName: "Procura por adoção de cães e gatos cresce na pandemia",
-                      ),
-                      onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext ctx) {
-                            return new ContentView();
-                          }))),
-                  Container(
-                    width: 30,
-                  ),
-                  InkWell(
-                      child: Content(
-                        topicName: "Economia",
-                        newsName: "Bovespa começa maio operando em forte queda",
-                        imgUrl: "https://s2.glbimg.com/XoRB1EVE2_MWoxxhAlPDLQznraI=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/W/7/RaokEcQPefLb2yrqIAwQ/kxilq-varia-o-do-ibovespa-em-2020-3-.png",
-                        color: Colors.amber,
-                      ),
-                      onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext ctx) {
-                            return new ContentView();
-                          }))),
-                  InkWell(
-                      child: Content(
-                        topicName: "corona\nvirus",
-                        newsName: "Aldir Blanc, compositor e escritor, morre no Rio",
-                        imgUrl: "https://s2.glbimg.com/4tXSL6-EO_dfJ5XUDudezH66R58=/240x135/top/smart/filters:strip_icc()/s2.glbimg.com/OhWUJMxguwxKPQwIeIsLlfLPc38=/0x117:2598x1576/267x150/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/g/m/u5APd4RnmbE0dmIpMLiw/age20200415011.jpg",
-                        color: Colors.redAccent,
-                      ),
-                      onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext ctx) {
-                            return new ContentView();
-                          }))),
-                  InkWell(
-                      child: Content(
-                        topicName: "bbb",
-                        newsName: "Ex-BBB Daniel fala da ansiedade pela primeira vez com Marcela",
-                        imgUrl: "https://s2.glbimg.com/44ef-vyFmWxoXbeSTCSbGQYisjY=/398x224/top/smart/filters:strip_icc()/s2.glbimg.com/3kkcyXQ8iwksilyJdTCc2O3Sy6s=/0x76:1280x797/398x224/i.glbimg.com/og/ig/infoglobo/f/original/2020/04/30/img-20200428-wa0073.jpg",
-                        color: Colors.deepOrange,
-
-                      ),
-                      onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext ctx) {
-                            return new ContentView();
-                          }))),
-                  InkWell(
-                      child: Content(
-                        topicName: "Mundo",
-                        newsName: "Remessas de brasileiros nos EUA caem até 90%, mas podem aliviar auge da crise no Brasil",
-                        imgUrl: "https://s2.glbimg.com/6qVfB55S2Zw8oozAz5hc_auDdyk=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2016/2/y/g2Ek6eRXqOIBr3UBgC7A/2016-12-14t130811z-1-lynxmpecbd0td-rtroptp-3-brazil-ratings-economy.jpg",
-                        color: Colors.blue,
-                      ),
-                      onTap: () => Navigator.of(context).push(
-                          MaterialPageRoute(builder: (BuildContext ctx) {
-                            return new ContentView();
-                          }))),
-                  Container(height: 5,),
-                  Container(
-                      color: Colors.blue,
-                      height: 80,
-                      child: Stack(
-                        children: <Widget>[
-                          Container(padding: EdgeInsets.only(top: 15),
-                            alignment: Alignment.topCenter,
-                            child: Text.rich(TextSpan(text:'globo.com', style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold))),),
-                          Container( padding: EdgeInsets.only(top: 47),
-                            alignment: Alignment.topCenter,
-                            child: Text.rich(TextSpan(text: '© Copyright 2000-2020 Globo Comunicação e Participações S.A.', style: TextStyle(fontSize: 10, color: Colors.white))),)
-                        ],
-                      )),
-                ],
-              ),
-            ),
-          )
-        ],
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => MyHomePage()));
+        },
       ),
-      resizeToAvoidBottomInset: true,
-      drawer: Drawer(
-        child: Text('DRAWER'),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/src/widgets/content.dart
+++ lib/src/widgets/content.dart
@@ -4,10 +4,15 @@ import 'package:carousel_slider/carousel_slider.dart';
 class MainContent extends StatefulWidget {
   final String topicName, newsName, imgUrl;
 
-  MainContent({this.topicName, this.newsName, this.imgUrl = "https://diariodamanha.com/wp-content/uploads/2019/02/doguinho-esta.jpg"});
+  MainContent(
+      {this.topicName,
+      this.newsName,
+      this.imgUrl =
+          "https://diariodamanha.com/wp-content/uploads/2019/02/doguinho-esta.jpg"});
 
   @override
-  _MainContentState createState() => _MainContentState(this.newsName, this.topicName, this.imgUrl);
+  _MainContentState createState() =>
+      _MainContentState(this.newsName, this.topicName, this.imgUrl);
 }
 
 class _MainContentState extends State<MainContent> {
@@ -26,51 +31,61 @@ class _MainContentState extends State<MainContent> {
         //alignment: Alignment.topLeft,
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: 10, top:10),
+            padding: EdgeInsets.only(left: 10, top: 10),
             alignment: Alignment.topLeft,
             child: Text.rich(TextSpan(
                 style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 23,
+                  color: Colors.black,
+                  fontSize: 23,
                   fontFamily: 'OpenSans',
                 ),
-                text: topicName.toUpperCase()
-            )),
+                text: topicName.toUpperCase())),
             height: 50,
           ),
-          Container(width: 20,),
           Container(
-            height:250,
+            width: 20,
+          ),
+          Container(
+            height: 250,
             child: CarouselSlider.builder(
                 itemCount: 5,
                 itemBuilder: (BuildContext context, int itemIndex) => Container(
-                    //padding: EdgeInsets.all(),
-                    child:
-                    Stack(children: <Widget>[
-                      ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                          child: Image.network(imgUrl,
-                            width: 800,
-                            height: 300,)
+                      //padding: EdgeInsets.all(),
+                      child: Stack(
+                        children: <Widget>[
+                          ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50.0)),
+                              child: Image.network(
+                                imgUrl,
+                                width: 800,
+                                height: 300,
+                              )),
+                          Container(
+                            margin: EdgeInsets.only(top: 150),
+                            padding: EdgeInsets.only(left: 10),
+                            decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(25),
+                                    bottomRight: Radius.circular(25))),
+                            //width: 300,
+                            height: 85,
+                            child: Text.rich(TextSpan(
+                                text: newsName + "\n",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold))),
+                            alignment: Alignment.bottomCenter,
+                          ),
+                        ],
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top:150),
-                        padding: EdgeInsets.only(left: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.black54,
-                          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25))
-                        ),
-                        //width: 300,
-                          height: 85,
-                          child: Text.rich(TextSpan(text: newsName +  "\n", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold))),
-                        alignment: Alignment.bottomCenter,
-                      ),
-                    ],),
-                ),
+                    ),
                 options: CarouselOptions(
                   height: 300,
                   viewportFraction: 0.80,
-                  aspectRatio: 16/9,
+                  aspectRatio: 16 / 9,
                   initialPage: 0,
                   enableInfiniteScroll: true,
                   reverse: false,
@@ -90,10 +105,16 @@ class Content extends StatefulWidget {
   final String topicName, newsName, imgUrl;
   final Color color;
 
-  Content({this.topicName, this.newsName, this.color, this.imgUrl = "https://diariodamanha.com/wp-content/uploads/2019/02/doguinho-esta.jpg"});
+  Content(
+      {this.topicName,
+      this.newsName,
+      this.color,
+      this.imgUrl =
+          "https://diariodamanha.com/wp-content/uploads/2019/02/doguinho-esta.jpg"});
 
   @override
-  _ContentState createState() => _ContentState(this.topicName, this.newsName, this.color, this.imgUrl);
+  _ContentState createState() =>
+      _ContentState(this.topicName, this.newsName, this.color, this.imgUrl);
 }
 
 class _ContentState extends State<Content> {
@@ -104,77 +125,81 @@ class _ContentState extends State<Content> {
     this.topicName = topicName;
     this.newsName = newsName;
     this.imgUrl = imgUrl;
-    this.color  = color;
+    this.color = color;
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-    Container(padding: EdgeInsets.only(top: 0), child:
-      Stack(
-      children: <Widget>[
-            Container(
-              width: 420,
-              padding: EdgeInsets.only(left: 40),
-              height:200,
-              child: CarouselSlider.builder(
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int itemIndex) => Container(
-                      child: Stack(children: <Widget>[
-                        ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                            child: Image.network(imgUrl,
-                              width: 320,
-                              height: 260,)
+    return Container(
+      padding: EdgeInsets.only(top: 0),
+      child: Stack(children: <Widget>[
+        Container(
+          width: 420,
+          padding: EdgeInsets.only(left: 40),
+          height: 200,
+          child: CarouselSlider.builder(
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int itemIndex) => Container(
+                      child: Stack(
+                    children: <Widget>[
+                      ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          child: Image.network(
+                            imgUrl,
+                            width: 320,
+                            height: 260,
+                          )),
+                      Container(
+                        padding: EdgeInsets.only(left: 7, top: 7),
+                        margin: EdgeInsets.only(top: 137),
+                        decoration: BoxDecoration(
+                          color: Colors.black54,
                         ),
-                        Container(
-                          padding: EdgeInsets.only(left: 7, top: 7),
-                          margin: EdgeInsets.only(top:137),
-                          decoration: BoxDecoration(
-                              color: Colors.black54,
-                          ),
-                          //width: 300,
-                          height: 60,
-                          child: Text.rich( TextSpan(text: newsName + "\n", style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold))),
-                          alignment: Alignment.bottomCenter,
-                        ),
-                      ],)
-                  ),
-                  options: CarouselOptions(
-                    height: 700,
-                    viewportFraction: 0.68,
-                    initialPage: 0,
-                    enableInfiniteScroll: false,
-                    reverse: false,
-                    autoPlay: false,
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enlargeCenterPage: false,
-                    scrollDirection: Axis.horizontal,
+                        //width: 300,
+                        height: 60,
+                        child: Text.rich(TextSpan(
+                            text: newsName + "\n",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold))),
+                        alignment: Alignment.bottomCenter,
+                      ),
+                    ],
                   )),
-            ),
+              options: CarouselOptions(
+                height: 700,
+                viewportFraction: 0.68,
+                initialPage: 0,
+                enableInfiniteScroll: false,
+                reverse: false,
+                autoPlay: false,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: false,
+                scrollDirection: Axis.horizontal,
+              )),
+        ),
         Container(
           padding: EdgeInsets.only(top: 23),
           child: Container(
-          padding: EdgeInsets.only(bottom: 10),
-          alignment: Alignment.bottomCenter,
-          child: Text.rich(TextSpan(
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Helvetica Neue'
-              ),
-
-              text: topicName.toUpperCase()
+            padding: EdgeInsets.only(bottom: 10),
+            alignment: Alignment.bottomCenter,
+            child: Text.rich(
+              TextSpan(
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Helvetica Neue'),
+                  text: topicName.toUpperCase()),
+              textDirection: TextDirection.rtl,
+            ),
+            height: 180,
+            width: 92,
+            decoration: BoxDecoration(
+                color: color, borderRadius: BorderRadius.circular(5)),
           ),
-            textDirection: TextDirection.rtl,),
-          height: 180,
-          width: 92,
-          decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(5)),
-        ),),
-
-    ]
-    ),);
+        ),
+      ]),
+    );
   }
 }
